91add96af9aadca5d842748acd68a13c6e398c7d
Clayton Zambon <zambon@outlook.com.br>
2023-10-26 23:14:08 -0300

    FEAT: Usando metodos

--- conta.py
+++ conta.py
@@ -6,3 +6,12 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de R${} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(selfself, valor):
+        self.saldo -= valor
