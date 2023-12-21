c4df91818d7b1565a3f848fb397fbdeef78b0aa1
Clayton Zambon <zambon@outlook.com.br>
2023-12-21 16:01:54 -0300

    FEAT: Relembrando classes e objetos - Parte 03

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -3,5 +3,13 @@ class Filme:
         self.nome = nome
         self.ano = ano
         self.duracao = duracao
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
 print(vingadores.nome)
+
+friends = Serie('Friends', 1995, 12)
+print(friends.nome)
