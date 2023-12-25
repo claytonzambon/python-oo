8df9ff87fac80fa7c74777f060746a5faa161b88
Clayton Zambon <zambon@outlook.com.br>
2023-12-25 18:21:52 -0300

    FEAT: Adicionando atributos e metodos - Parte 01

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -3,15 +3,24 @@ class Filme:
         self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
 class Serie:
     def __init__(self, nome, ano, temporadas):
         self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
 
+    def dar_likes(self):
+        self.likes += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 friends = Serie('friends', 1995, 12)
-print (f'Nome: {friends.nome} - Ano: {friends.ano} - Temporadas: {friends.temporadas}')
+print (f'Nome: {friends.nome} - Ano: {friends.ano} - Temporadas: {friends.temporadas} - Likes: {friends.likes}')
