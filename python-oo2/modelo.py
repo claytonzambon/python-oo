95b14cc2f285b84dc7177c3561ffe69cf5be90c9
Clayton Zambon <zambon@outlook.com.br>
2023-12-26 22:18:21 -0300

    FEAT: Encapsulando comportamento

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -1,25 +1,45 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
 
+    @property
+    def nome(self):
+        return self.__nome
 
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
 
-    def dar_likes(self):
-        self.likes += 1
+    @property
+    def likes(self):
+        return self.__likes
 
+    def dar_like(self):
+        self.__likes += 1
 
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
@@ -27,8 +47,8 @@ vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 friends = Serie('friends', 1995, 12)
-friends.dar_likes()
-friends.dar_likes()
+friends.dar_like()
+friends.dar_like()
 
 print (f'Nome: {friends.nome} - Ano: {friends.ano} - Temporadas: {friends.temporadas} - Likes: {friends.likes}')
 
