4ff05b2932ca3f9c0c96a458c9ebc3235fa001b4
Clayton Zambon <zambon@outlook.com.br>
2024-01-08 02:35:37 -0300

    FEAT: Fugindo da complexidade

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -37,11 +37,18 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
 
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return  len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 friends = Serie('friends', 1995, 12)
@@ -63,9 +70,7 @@ filmes_e_series = [vingadores, friends, seinfield, carros]
 
 playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
-print(f'Tá ou não tá? {friends in playlist_fim_de_semana}')
