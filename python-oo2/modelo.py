cf1975bc1f64b644c76138fa43aa78b7674f1441
Clayton Zambon <zambon@outlook.com.br>
2024-01-09 20:46:56 -0300

    FEAT: Duck Typing

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -42,6 +42,9 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return  self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
@@ -72,5 +75,7 @@ playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
 print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
-for programa in playlist_fim_de_semana.listagem:
+print (vingadores in playlist_fim_de_semana)
+
+for programa in playlist_fim_de_semana:
     print(programa)
