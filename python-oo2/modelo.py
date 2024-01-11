6a3cffa064d6826e2a8b8f5422ba8239e9fdac7e
Clayton Zambon <zambon@outlook.com.br>
2024-01-10 23:16:54 -0300

    FEAT: Modelo de dados Python - Parte 01

--- python-oo2/modelo.py
+++ python-oo2/modelo.py
@@ -49,8 +49,7 @@ class Playlist:
     def listagem(self):
         return self._programas
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return  len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -73,7 +72,7 @@ filmes_e_series = [vingadores, friends, seinfield, carros]
 
 playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
 print (vingadores in playlist_fim_de_semana)
 
